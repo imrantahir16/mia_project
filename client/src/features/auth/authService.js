a8d3cd9dcbdd3d5b51f64b7cf5e8586870b50fed
imrantahir16 <imrantahir16@gmail.com>
2023-05-27 01:19:12 +0500

    change base url

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "../../api/axios";
 
-const API_URL = "api/auth/";
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}api/auth/`;
 
 // register user
 const register = async (userData) => {
