76594af4237edc022d655951eeb6533208fbc7cb
imrantahir16 <imrantahir16@gmail.com>
2023-05-28 00:03:42 +0500

    add domain in cors

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  // "https://social-app-bice.vercel.app/",
+  " http://api.ilikemia.com/",
+  " http://www.ilikemia.com/",
   "http://localhost:8080/",
   "http://localhost:3000/",
   "http://127.0.0.1:3000/",
