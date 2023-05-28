075df913d97d0d86fd2d36eeb7a86ef0fcfe202a
imrantahir16 <imrantahir16@gmail.com>
2023-05-28 13:18:32 +0500

    cors origin

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-  " http://api.ilikemia.com/",
-  " http://www.ilikemia.com/",
+  "http://www.ilikemia.com/",
   "http://localhost:8080/",
   "http://localhost:3000/",
   "http://127.0.0.1:3000/",
